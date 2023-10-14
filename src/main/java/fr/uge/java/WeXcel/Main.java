a319d4ff15af325266d51d6cdfa9d01f6da6461d
Ilyass Berradi <berradi.ilyass2@gmail.com>
2023-10-14 20:29:53 +0200

    Added connection with database

---
+++ src/main/java/fr/uge/java/WeXcel/Main.java
@@ -0,0 +1,19 @@
+package fr.uge.java.WeXcel;
+
+import io.helidon.microprofile.server.Server;
+import java.io.IOException;
+
+public final class Main {
+
+    private Main() { }
+
+    public static void main(final String[] args) throws IOException {
+        Server server = startServer();
+        System.out.println("http://localhost:" + server.port() + "/greet");
+    }
+
+    static Server startServer() {
+        return Server.create().start();
+    }
+
+}
