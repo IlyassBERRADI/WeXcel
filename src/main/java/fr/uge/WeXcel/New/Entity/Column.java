89a1453ed63cb0287b12962060c56635310bc951
Marius Lemaire <mlemai13@etud.u-pem.fr>
2023-11-30 00:12:47 +0100

    Column est mtn une classe pour gérer son type dynamiquement -> Jackson serializable and deserializable compatible addColumn ok tout ca tout ca

--- src/main/java/fr/uge/WeXcel/New/Entity/Column.java
+++ src/main/java/fr/uge/WeXcel/New/Entity/Column.java
@@ -1,14 +1,51 @@
 package fr.uge.WeXcel.New.Entity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public record Column(String name, String type, List<String> values) {
+public class Column {
+    private String name;
+    private String type;
+    private List<String> values;
 
-    public Column{
-        Objects.requireNonNull(name);
-        Objects.requireNonNull(type);
+    // Constructeur par défaut nécessaire pour JPA
+    public Column() {
 
     }
+    public Column(String name, String type) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(type);
+        this.name = name;
+        this.type = type;
+        this.values = new ArrayList<>();
+    }
+    public Column(String name, String type, List<String> values) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(values);
+        this.name = name;
+        this.type = type;
+        this.values = values;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+    public List<String> getValues() {
+        return new ArrayList<>(values);
+    }
+    public String getValue(int index) {
+        return values.get(index);
+    }
 
+    public void addValue(String value) {
+        values.add(value);
+    }
+
+    public int size() {
+        return values.size();
+    }
 }
